667ff113b17aa5900ccef01dfa2018a13d68c950
ZiyiLiang <ziyilian@usc.edu>
2023-10-04 22:08:42 -0700

    reorganized the repo and moved functions for the old cmc project to the archived folder

---
+++ pairedRS/models.py
@@ -0,0 +1,145 @@
+import numpy as np   
+from scipy.stats import ortho_group
+
+
+class RandomFactorizationModel():
+    def __init__(self, m, n, r):
+        self.m = m   # shape of the output matrix
+        self.n = n
+        self.r = r   # matrix rank
+    
+    def sample_noiseless(self, random_state=0):
+        """
+        Generate noiseless target matrix by sampling factor matrices from 
+        standard normal distribution
+        """
+        np.random.seed(random_state)
+        U = np.random.randn(self.m, self.r)
+        V = np.random.randn(self.n, self.r)
+        M = U @ V.T
+        return U, V, M
+
+    def sample_noisy(self, sigma, random_state=0): 
+        """
+        Generate noisy target matrix by adding a random gaussian noise to the 
+        noiseless matrix
+
+        Args
+        ------
+        sigma: noise level
+        """
+        U, V, M = self.sample_noiseless(random_state=random_state)
+        noisy_M = np.random.randn(self.m, self.n) * sigma + M
+        return U, V, M, noisy_M
+
+
+class RandomOrthogonalModel():
+    """ 
+    This class implements the random orthogonal model described by Candes and
+    Recht (2008) which satisfies the incoherence condition.
+    """
+    def __init__(self, m, n, r):
+        self.m = m   # shape of the output matrix
+        self.n = n
+        self.r = r   # matrix rank
+
+    def sample_noiseless(self, random_state):
+        """
+        Generate noiseless target matrix by sampling factor matrices from 
+        r latent orthonormal vectors 
+        """
+        np.random.seed(random_state)
+        U = ortho_group.rvs(self.m)[:self.r].T
+        V = ortho_group.rvs(self.n)[:self.r].T
+        S = np.diag(np.random.uniform(-2 * max(self.m, self.n), 
+                                      2 * max(self.m, self.n), self.r))
+        M = U @ S @ V.T
+        return U, V, S, M
+    
+    def sample_noisy(self, sigma, random_state):
+        """
+        Generate noisy target matrix by sampling factor matrices from 
+        k latent orthonormal vectors plus gaussian noise
+        """
+        U, V, S, M = self.sample_noiseless(random_state=random_state)
+        noisy_M = np.random.randn(self.m, self.n) * sigma + M
+        return U, V, S, M, noisy_M
+
+
+
+class PairSampling():
+    """ 
+    This class implements functions to sample observed index set and perform data
+    splitting 
+    """
+    def __init__(self, n1, n2):
+        self.n1 = n1   # shape of the output matrix
+        self.n2 = n2
+        self.shape = (self.n1, self.n2)
+        
+    def sample_submask(self, sub_size, mask=None, w=None, random_state=0):
+        rng = np.random.default_rng(random_state)
+        
+        if mask is None:
+            mask = np.ones(self.shape)
+        mask = mask.flatten(order='C')   # flatten the matrix by rows
+        obs_idx = np.where(mask == 1)[0]
+        sub_size = int(np.clip(sub_size, 1,len(obs_idx)-1))
+
+        if w is not None:
+            w = w.flatten(order='C')  # flatten the matrix by rows    
+        else:
+            w = np.array([1] * (self.n1 * self.n2)) / (self.n1 * self.n2)
+        
+        # Make sure the weights sum up to 1 at selected indexes
+        w = w / np.sum(w[obs_idx])
+        sub_idx = rng.choice(obs_idx, sub_size, replace=False, p=w[obs_idx])
+
+        sub_mask = np.zeros(self.n1 * self.n2)
+        sub_mask[sub_idx] = 1
+        return sub_mask.reshape(self.shape)
+    
+    def sample_train_calib(self, mask_obs, n_pairs, random_state=0):
+        rng = np.random.default_rng(random_state)
+        
+        mask_drop = np.zeros_like(mask_obs)
+        mask_train = np.zeros_like(mask_obs)
+        mask_calib = np.zeros_like(mask_obs)
+        n_obs = np.sum(mask_obs, axis=1)   # Check the observation# in each row
+        
+        assert n_pairs <= np.sum(n_obs // 2), "Too many calibration pairs and not enough observations!"
+        idxs_calib = (-np.ones(2 * n_pairs, dtype=int),\
+                      -np.ones(2 * n_pairs, dtype=int))
+        
+        shuffled_pool = {}
+        for i in range(self.n1):
+            if n_obs[i] > 0:
+                idxs = np.where(mask_obs[i]==1)[0]
+                rng.shuffle(idxs)
+                if n_obs[i] % 2 != 0:
+                    # If the row length is odd, take the last shuffled index as the dropout
+                    mask_drop[i][idxs[-1]] = 1
+                    idxs = idxs[:-1]
+                if len(idxs) > 0:
+                    shuffled_pool[i] = idxs
+                    
+        
+        # Sample calibration pairs
+        for i in range(n_pairs):
+            row = rng.choice(list(shuffled_pool.keys()))  # Randomly select a row
+            
+            # First two shuffled entries form a pair
+            idxs_calib[0][2 * i], idxs_calib[0][2 * i + 1] = row, row
+            idxs_calib[1][2 * i], idxs_calib[1][2 * i + 1] = shuffled_pool[row][0], shuffled_pool[row][1]
+            
+            # Update the shuffled pool by removing the selected pair
+            if len(shuffled_pool[row]) == 2:
+                del shuffled_pool[row]
+            else:
+                shuffled_pool[row] = shuffled_pool[row][2:]
+                
+        # Get the training mask
+        mask_calib[idxs_calib] = 1
+        mask_train = mask_obs - mask_calib
+        return mask_train, mask_calib, mask_drop, idxs_calib
+    
---
+++ pairedRS/utils.py
@@ -0,0 +1,82 @@
+import numpy as np   
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def plot_before_after_mask(M, mask, vmin=-5, vmax=5):
+    # Plot the masked and unmasked matrix side by side
+    fig, axs = plt.subplots(1,2, figsize=(12,5))
+    fig.tight_layout()
+    plt.subplots_adjust(wspace=0.05)
+
+    # imshow plots matrices 
+    im1 = axs[0].imshow(M, vmin=vmin, vmax=vmax)
+    axs[0].title.set_text('Ground truth matrix')
+    plt.colorbar(im1, ax=axs[0], fraction=0.046, pad=0.04)
+
+    plot_masked = -100*(1-mask) + np.multiply(M,mask)
+    im2 = axs[1].imshow(plot_masked,vmin=vmin, vmax=vmax)
+    axs[1].title.set_text('Masked matrix')
+    plt.colorbar(im2, ax=axs[1], fraction=0.046, pad=0.04)
+    plt.show()
+
+
+def evaluate_mse(M, Mhat, idx):
+    """
+    Calculate RMSE on the given index set, for true matrix UVᵀ.
+    
+    Args
+    ------
+    M:      Ground Truth matrix
+    M_hat:  Estimated matrix 
+    idx:    Binary matrix where target indexed are labelled 1, 0 otherwise
+    
+    Returns:
+    --------
+    rmse:   Mean squared error over all unobserved entries
+    """
+    pred = np.multiply(Mhat, idx)
+    truth = np.multiply(M, idx)
+    cnt = np.sum(idx)
+    return np.linalg.norm(pred - truth, "fro") ** 2 / cnt
+
+def theoretical_bound(m, n, r, C=1):
+    """ 
+    Approximates the minimal training samples needed for exact matrix recovery,
+    following the results from Theoem 1.1 in Candes and Recht[2008].
+    """
+
+    n = max(m, n)
+    if r <= n ** (1 / 5):
+        m = C * n ** (6 / 5) * r * np.log(n)
+    else:
+        m = C * n ** (5 / 4) * r * np.log(n)
+
+    return np.ceil(m)
+
+
+def evaluate_CI(lower, upper, M, idxs_test, label=None):
+    label = str(label)+'_' if label else ''
+    val = M[idxs_test]
+    coverage = np.mean((lower < val) & (upper > val))
+    size = np.mean(upper - lower)
+    
+    results = pd.DataFrame({})
+    results[label+"Coverage"] = [coverage]
+    results[label+"Size"] = [size]
+    return results
+
+def evaluate_pairedCI(lower, upper, M, idxs_test, label=None):
+    label = str(label)+'_' if label else ''
+    val = M[idxs_test]
+    n_test_pair = len(idxs_test[0]) // 2
+    
+    covered = (lower < val) & (upper > val)
+    pair_covered = [covered[2*i] * covered[2*i+1] for i in range(n_test_pair)]
+    
+    coverage = np.mean(pair_covered)
+    size = np.mean(upper - lower)
+    results = pd.DataFrame({})
+    results[label+"Coverage"] = [coverage]
+    results[label+"Size"] = [size]
+    return results
