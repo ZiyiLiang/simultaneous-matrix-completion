ed298b2afb89bfd5b89cdcd7c9e20c738e988eb7
ZiyiLiang <ziyilian@usc.edu>
2023-10-26 21:26:54 -0700

    [tmp] added working experiments of bonferroni bound on hpm

--- experiments_synthetic/make_plots_hpm.R
+++ experiments_synthetic/make_plots_hpm.R
@@ -31,5 +31,7 @@ results_hpm <- results.raw %>%
   group_by(Method, n1, n2, r_true, r_guess, prob_obs, Key) %>%
   summarise(num=n(), Value.se = sd(Value, na.rm=T)/sqrt(n()), Value=mean(Value, na.rm=T))%>%
 #  pivot_wider(names_from = 'Key', values_from = 'Value') %>%
-  filter(n1 == 500, n2 == 500, r_true == 20, r_guess==20)
+  filter(n1 == 100, n2 == 100, prob_obs==0.3, r_true == 20, r_guess==20)
 
+results_filtered <- results.raw %>%
+  filter(n1 == 100, n2 == 100, prob_obs==0.3, r_true == 20, r_guess==20)
