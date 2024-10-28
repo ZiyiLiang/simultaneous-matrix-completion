bbc2aa2b31c5db8ac187a66c3fb4d63ac852dfc4
ZiyiLiang <ziyilian@usc.edu>
2024-10-28 12:16:44 -0700

    modified submit_exp_uniform file to see how guessed rank changes the prediction performance

--- experiments_synthetic/submit_exp_uniform.sh
+++ experiments_synthetic/submit_exp_uniform.sh
@@ -3,10 +3,10 @@
 # Parameters
 N1=200
 N2=200
-R=5
-MU_LIST=$(seq 0 3 30)
-SEED_LIST=$(seq 101 150)
-#MU_LIST=(30)
+R_LIST=(1 2 3 4 6 8 10)
+#MU_LIST=$(seq 0 3 30)
+SEED_LIST=$(seq 1 50)
+MU_LIST=(30)
 #SEED_LIST=(0)
 
 # Slurm parameters
@@ -28,29 +28,31 @@ OUT_DIR="results/exp_uniform"
 mkdir -p $OUT_DIR
 for SEED in $SEED_LIST; do
     for MU in $MU_LIST; do
-        JOBN=$N1"by"$N2"_r"$R"_mu"$MU"_seed"$SEED
-        OUT_FILE=$OUT_DIR"/"$JOBN".txt"
-        COMPLETE=0
-        #ls $OUT_FILE
-        if [[ -f $OUT_FILE ]]; then
-        COMPLETE=1
-        ((comp++))
-        fi
+        for R in "${R_LIST[@]}"; do
+            JOBN=$N1"by"$N2"_r"$R"_mu"$MU"_seed"$SEED
+            OUT_FILE=$OUT_DIR"/"$JOBN".txt"
+            COMPLETE=0
+            #ls $OUT_FILE
+            if [[ -f $OUT_FILE ]]; then
+            COMPLETE=1
+            ((comp++))
+            fi
 
-        if [[ $COMPLETE -eq 0 ]]; then
-        ((incomp++))
-	    # Script to be run
-        SCRIPT="exp_uniform.sh $N1 $N2 $R $MU $SEED"
-        # Define job name
-        OUTF=$LOGS"/"$JOBN".out"
-        ERRF=$LOGS"/"$JOBN".err"
-        # Assemble slurm order for this job
-        ORD=$ORDP" -J "$JOBN" -o "$OUTF" -e "$ERRF" "$SCRIPT
-        # Print order
-        echo $ORD
-        # Submit order
-        $ORD
-        fi
+            if [[ $COMPLETE -eq 0 ]]; then
+            ((incomp++))
+            # Script to be run
+            SCRIPT="exp_uniform.sh $N1 $N2 $R $MU $SEED"
+            # Define job name
+            OUTF=$LOGS"/"$JOBN".out"
+            ERRF=$LOGS"/"$JOBN".err"
+            # Assemble slurm order for this job
+            ORD=$ORDP" -J "$JOBN" -o "$OUTF" -e "$ERRF" "$SCRIPT
+            # Print order
+            echo $ORD
+            # Submit order
+            $ORD
+            fi
+        done
     done
 done
 
