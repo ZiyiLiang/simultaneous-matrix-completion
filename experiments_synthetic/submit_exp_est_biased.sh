3f36ecedd2a0efb42de2f2c82aa34b835be7d1c4
ZiyiLiang <ziyilian@usc.edu>
2024-11-06 17:15:01 -0800

    fixed small bug in submission file for exp est bised

--- experiments_synthetic/submit_exp_est_biased.sh
+++ experiments_synthetic/submit_exp_est_biased.sh
@@ -5,7 +5,7 @@
 #SCALE_LIST=(1)
 SCALE_LIST=$(seq 0.6 0.05 1.0)
 #SCALE_LIST=$(seq 0 2 10)
-CONST_LIST=c(10 20 30 50 70)
+CONST_LIST=(10 20 30 50 70)
 #SEED_LIST=(1)
 SEED_LIST=$(seq 1 10)
 #R_LIST=(1)
@@ -31,31 +31,33 @@ incomp=0
 OUT_DIR="results/exp_est_biased"
 mkdir -p $OUT_DIR
 for SEED in $SEED_LIST; do
-    for SCALE in $SCALE_LIST; do
-        for R in "${R_LIST[@]}"; do
-            JOBN="r"$R"_const"$CONST"_scale"$SCALE"_seed"$SEED
-            OUT_FILE=$OUT_DIR"/"$JOBN".txt"
-            COMPLETE=0
-            #ls $OUT_FILE
-            if [[ -f $OUT_FILE ]]; then
-            COMPLETE=1
-            ((comp++))
-            fi
+    for CONST in "${CONST_LIST[@]}"; do
+        for SCALE in $SCALE_LIST; do
+            for R in "${R_LIST[@]}"; do
+                JOBN="r"$R"_const"$CONST"_scale"$SCALE"_seed"$SEED
+                OUT_FILE=$OUT_DIR"/"$JOBN".txt"
+                COMPLETE=0
+                #ls $OUT_FILE
+                if [[ -f $OUT_FILE ]]; then
+                COMPLETE=1
+                ((comp++))
+                fi
 
-            if [[ $COMPLETE -eq 0 ]]; then
-            ((incomp++))
-            # Script to be run
-            SCRIPT="exp_est_biased.sh $R $CONST $SCALE $SEED"
-            # Define job name
-            OUTF=$LOGS"/"$JOBN".out"
-            ERRF=$LOGS"/"$JOBN".err"
-            # Assemble slurm order for this job
-            ORD=$ORDP" -J "$JOBN" -o "$OUTF" -e "$ERRF" "$SCRIPT
-            # Print order
-            echo $ORD
-            # Submit order
-            $ORD
-            fi
+                if [[ $COMPLETE -eq 0 ]]; then
+                ((incomp++))
+                # Script to be run
+                SCRIPT="exp_est_biased.sh $R $CONST $SCALE $SEED"
+                # Define job name
+                OUTF=$LOGS"/"$JOBN".out"
+                ERRF=$LOGS"/"$JOBN".err"
+                # Assemble slurm order for this job
+                ORD=$ORDP" -J "$JOBN" -o "$OUTF" -e "$ERRF" "$SCRIPT
+                # Print order
+                echo $ORD
+                # Submit order
+                $ORD
+                fi
+            done
         done
     done
 done
