1f6a2af896f6a3ccdc987851ce9b1ccb701772d2
ZiyiLiang <ziyilian@usc.edu>
2024-12-15 22:49:54 -0800

    small changes

--- experiments_synthetic/submit_exp_solver_uniform.sh
+++ experiments_synthetic/submit_exp_solver_uniform.sh
@@ -3,14 +3,13 @@
 # Parameters
 #MU_LIST=(0)
 MU_LIST=(15)
-SEED_LIST=$(seq 1 300)
+SEED_LIST=$(seq 1 150)
 SOLVER_LIST=("svt" "nnm")
-#SOLVER_LIST=("svt" "nnm")
 #SEED_LIST=(1)
 
 # Slurm parameters
 MEMO=1G                             # Memory required (1 GB)
-TIME=00-00:30:00                    # Time required (2 h)
+TIME=00-01:00:00                    # Time required (2 h)
 CORE=1                              # Cores required (1)
 
 # Assemble order                                               prefix
