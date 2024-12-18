7d2f53168493d2ad770443402ee36ece0f82e158
ZiyiLiang <ziyilian@usc.edu>
2024-12-17 22:21:03 -0800

    added more repetitions for exp movielens conditional

--- experiments_real/submit_exp_movielens_conditional.sh
+++ experiments_real/submit_exp_movielens_conditional.sh
@@ -3,7 +3,7 @@
 # Parameters
 #SCALE_LIST=$(seq 0 0.5 2)
 GENRE_LIST=("Drama" "Comedy" "Thriller" "Romance" "Action" "Adventure" "Children's" "Crime" "Horror" "Sci-Fi" "War" "Musical" "Documentary" "Mystery") 
-SEED_LIST=$(seq 1 30)
+SEED_LIST=$(seq 31 150)
 #SEED_LIST=(1)
 #CAL_LIST=(2000 3000)
 CAL_LIST=(2000)
