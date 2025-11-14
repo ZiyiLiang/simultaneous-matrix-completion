e2bd0889fcbfaf5acdbfe02e205aca5deaa6d294
Cora Liang <liangziyi0521@gmail.com>
2025-11-14 11:21:32 -0800

    working progres

--- experiments_real/make_plot_largescale.R
+++ experiments_real/make_plot_largescale.R
@@ -17,11 +17,12 @@ results.raw <- do.call("rbind", lapply(ifile.list, function(ifile) {
 results.raw$t_test <- results.raw$t_inference-results.raw$t_universal
 
 key.values <- c("t_train", "t_missing", "t_sample_calib", "t_test", "t_universal")
-key.labels <- c("Pred. model", "Miss. model", "Calib. sampling", "Test inference", "One-time comp.")
+key.labels <- c("Point pred.", "Missingness est.", "Calib. sampling", "Test inference", "One-time comp.")
 
 results <- results.raw %>%
-  pivot_longer(cols=c("t_train", "t_missing", "t_sample_calib", "t_test", "t_universal"), names_to='Key', values_to='Value') %>%
+  pivot_longer(cols=c("t_train", "t_missing", "t_sample_calib", "t_universal",  "t_test"), names_to='Key', values_to='Value') %>%
   mutate(Key = factor(Key, key.values, key.labels)) %>%
   group_by(Calib_queries, k, Key) %>%
-  summarise(num=n(), Value.se = sd(Value, na.rm=T)/sqrt(n()), Value=mean(Value, na.rm=T))
+  summarise(num=n(), Value.se = sd(Value, na.rm=T)/sqrt(n()), Value=mean(Value, na.rm=T))%>%
+  filter(k==6)
 
